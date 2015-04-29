310eb17e5fe75f84e1cdff3738af35c0657d1af6
Axel Tanner <axeltanner38@gmail.com>
2015-04-29 11:59:36 -0700

    Finished quiz

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(".dan").on('click', function(){
+		alert("Hey you clicked on me");
+	});
+});
+
+$(document).ready(function(){
+	$("#iwao").on('click', function(){
+		$("#iwao").hide();
+	});
+});
